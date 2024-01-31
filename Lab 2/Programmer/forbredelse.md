7c86118e52c20bbe6611dcbf2e88f7868c2b5477
Christian Magnus Heimvik <cmheimvik@gmail.com>
2024-01-31 21:54:39 +0100

    Added more files

---
+++ Lab 2/Programmer/forbredelse.md
@@ -0,0 +1,8 @@
+# Oppgave 1
+Se forbredelse.py
+
+# Oppgave 2
+Vi har Ts=1/fs sekunder mellom hver sample, lyden bruker en tid d/c på distansen det. Maksimalt antall samples, som er tilfellet når lyden beveger seg hele distansen d, blir da ((d/c)/Ts).
+
+# Oppgave 3
+Flere unike innfalssvinkler vil kunne oppdages jo flere sampels vi har. Dette pga vi må avrunde til heltall når vi skifter. Vi kan forbedre dette ved å: lengre avstand mellom mikrofonene, høyere reell fs eller interpolere i etterkant.
---
+++ Lab 2/Programmer/forbredelse.py
@@ -0,0 +1,33 @@
+import scipy.signal as ss
+import numpy as np
+import matplotlib.pyplot as plt
+
+samples = 1000
+start = 0
+shift_1 = 0.2
+shift_2 = 0.5
+stop = 1
+A = 5
+l = 0.05
+
+fs = 10000
+
+#two signals
+t = np.linspace(start,stop,fs*(stop-start))
+
+x_1 = A*np.exp(-((t-shift_1)/l)**2)
+x_2 = A*np.exp(-((t-shift_2)/l)**2)
+
+correlation = ss.correlate(x_1,x_2)
+t_c = ss.correlation_lags(len(x_1),len(x_2))
+fig, ax = plt.subplots(2,1)
+ax[0].plot(t,x_1)
+ax[0].plot(t,x_2)
+ax[0].set_xlabel("Time [s]")
+ax[0].set_ylabel("Amplitude [V]")
+
+ax[1].plot(t_c/fs,correlation)
+ax[1].set_xlabel("Time [s]")
+ax[1].set_ylabel("Amplitude [V]")
+
+plt.show()
